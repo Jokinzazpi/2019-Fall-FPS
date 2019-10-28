8b27720ca91cde1a3c851423b02c878efa83e123
Mikel Fernandez <fernitxiki75@gmail.com>
2019-10-28 18:52:35 +0100

    .bat update

--- COPY_RESOURCES.bat
+++ COPY_RESOURCES.bat
@@ -1,2 +1,2 @@
 mkdir "2019-Fall-FPS\content\Resources"
-Xcopy /D /E /I "%USERPROFILE%\DigiPen Institute of Technology\Joaquin Bilbao - Content\Resources" "%cd%\content\Resources"
+Xcopy /D /E /I "%USERPROFILE%\DigiPen Institute of Technology\Joaquin Bilbao - Content\Resources" "%cd%2019-Fall-FPS\Astra\Content\Resources"
