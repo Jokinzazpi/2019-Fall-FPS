586191f0d4748500c5060ba07cfd0a90125e12db
Joaquin Bilbao <Jbilpas@gmail.com>
2019-11-20 12:57:26 +0100

    Moved the music system to the music manager

--- COPY_RESOURCES.bat
+++ COPY_RESOURCES.bat
@@ -1,2 +1,6 @@
 mkdir "Astra\Content\Resources"
 Xcopy /D /E /I "%USERPROFILE%\DigiPen Institute of Technology\Joaquin Bilbao - Content\Resources" "%cd%\Astra\Content\Resources"
+
+mkdir "Astra\Content\Resources"
+Xcopy /D /E /I "%USERPROFILE%\OneDrive - DigiPen Institute of Technology\Content\Resources" "%cd%\Astra\Content\Resources"
+
